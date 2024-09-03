0ac7148085c0e5701c626ccffda87c28000c4dca
Frank Affatigato <ifrankdavida@gmail.com>
2024-09-02 22:00:58 -0500

    Created repo for in memory vector store application

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings, OpenAI
+from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain import hub
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("hi")
+    pdf_path = "C:/Users/ifran/OneDrive/Desktop/Dev/GenAI/Udemy LangChain GenAI Course/vectorstore-in-memory/a88c5855-d533-490f-a9a5-ae916191c089.pdf"
+    loader = PyPDFLoader(file_path=pdf_path)
+    documents = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=30, separator="\n")
+    docs = text_splitter.split_documents(documents=documents)
+
+    embeddings = OpenAIEmbeddings()
+    vectorstore = FAISS.from_documents(docs, embeddings)
+    vectorstore.save_local("faiss_index_react")
+
+    new_vectorstore = FAISS.load_local(
+        "faiss_index_react", embeddings, allow_dangerous_deserialization=True
+    )
+
+    retrieval_qa_chat_prompt = hub.pull("langchain-ai/retrieval-qa-chat")
+    combine_docs_chain = create_stuff_documents_chain(
+        OpenAI(), retrieval_qa_chat_prompt
+    )
+
+    retrieval_chain = create_retrieval_chain(
+        new_vectorstore.as_retriever(), combine_docs_chain
+    )
+
+    res = retrieval_chain.invoke({"input": "Give me the gist of ReAct in 3 sentences"})
+    print(res["answer"])
+
+    #pull in the prompt that will be used for the rag chain.
---
+++ test.py
@@ -0,0 +1,4 @@
+import sys
+
+# Print the path of the Python executable
+print(f"Python executable is located at: {sys.executable}")
